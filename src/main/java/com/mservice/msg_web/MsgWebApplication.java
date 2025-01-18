79b99d1012ef345d9317789b3cc3980696a4600f
DAEHOCHANG <eogh1209@gmail.com>
2025-01-18 15:53:32 +0900

    spring boot 세팅

---
+++ src/main/java/com/mservice/msg_web/MsgWebApplication.java
@@ -0,0 +1,13 @@
+package com.mservice.msg_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mservice/msg_web/MsgWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mservice.msg_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
