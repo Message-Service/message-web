bf9c94406564b7d5b3e3e77c7f040b058e41f5ee
hojun3377 <hojun3377@gmail.com>
2025-01-19 10:33:12 +0900

    Feat: 타임리프 구성 및 테스트 화면 구성

---
+++ src/main/java/com/mservice/msg_web/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.mservice.msg_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("title", "test");
+        return "test/index";
+    }
+}
