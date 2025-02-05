22f0dc494d5dcb4bb85f064b557f421a21575a2c
hojun3377 <hojun3377@gmail.com>
2025-02-05 21:15:19 +0900

    [#2] [기반작업] 빌드, 배포 구성하기  - 브랜치 명 미기입 시 develop 브랜치에서 빌드

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
 # 환경변수 설정
-export APP_HOME="/root/message-web"
+
+# 모드 설정
 export APP_ENV="production"
 
+# Path 설정
+export APP_HOME="/root/message-web"
+export APP_SCRIPT_PATH="/root/scripts"
+
 # 적용 확인
-echo "APP_HOME: $APP_HOME"
 echo "APP_ENV: $APP_ENV"
+echo "APP_HOME: $APP_HOME"
+echo "APP_SCRIPT_PATH: $APP_SCRIPT_PATH"
